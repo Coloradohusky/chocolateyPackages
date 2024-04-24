74bfdeb7cc53ed3c3d6089844021b51b17fd0e40
Riley Nielsen <45430066+Coloradohusky@users.noreply.github.com>
2024-04-24 14:25:52 -0700

    upload ralibretro and resilio-sync-home

---
+++ ralibretro/tools/chocolateyInstall.ps1
@@ -0,0 +1,45 @@
+﻿$packageName   = 'ralibretro'
+$url           = 'https://retroachievements.org/bin/RALibretro.zip'
+$url64         = 'https://retroachievements.org/bin/RALibretro-x64.zip'
+$checksum      = '0EA1E31F67D169459FCB7FFBF47987F9EAADFB42FDE0134AC54836A0E8AB739F'
+$checksum64    = 'BE5339C5CC7ECEE627CCB825ED71D89002A7E16607E665DD4BF9EBA590862B0B'
+$checksumType  = 'sha256'
+$checksumType64= 'sha256'
+
+
+# Get the package parameters and then back them up for the uninstaller
+# This works around choco#1479 https://github.com/chocolatey/choco/issues/1479
+$pp = Get-PackageParameters
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$paramsFile = Join-Path (Split-Path -Parent $toolsPath) 'PackageParameters.xml'
+Write-Debug "Writing package parameters to $paramsFile"
+Export-Clixml -Path $paramsFile -InputObject $pp
+
+if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
+  $specificFolder = "RALibretro"
+} else {
+  $specificFolder = "RALibretro-x64"
+}
+
+$installDir = $(Get-ToolsLocation)
+if ($pp.InstallDir -or $pp.InstallationPath ) { $installDir = $pp.InstallDir + $pp.InstallationPath }
+Write-Host "RALibretro is going to be installed in '$installDir\$specificFolder'"
+
+Install-ChocolateyZipPackage "$packageName" `
+  -Url "$url" -Checksum "$checksum" -ChecksumType $checksumType `
+  -Url64 "$url64" -Checksum64 "$checksum64" -ChecksumType64 $checksumType64 `
+  -UnzipLocation "$installDir\$specificFolder"
+
+if ($installDir -eq $toolsPath) {
+  New-Item "$installDir\$specificFolder\RALibretro.exe.gui" -Type file -Force | Out-Null
+} else {
+  Install-BinFile ralibretro -path "$installDir\$specificFolder\RALibretro.exe" -UseStart
+}
+
+if ($pp.DesktopShortcut) {
+  $desktop = [System.Environment]::GetFolderPath("Desktop")
+  Install-ChocolateyShortcut -ShortcutFilePath "$desktop\RALibretro.lnk" `
+    -TargetPath "$installDir\$specificFolder\RALibretro.exe" -WorkingDirectory "$installDir" `
+    -WindowStyle 3
+  Write-Host "Added desktop shortcut at '$desktop\RALibretro.lnk'"
+}
---
+++ resilio-sync-home/tools/chocolateyInstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'resilio-sync-home'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://download-cdn.resilio.com/stable/windows/Resilio-Sync.exe'
+$checksum       = 'F68CE817832191F8B8C5FE5BFCAA47DCBA548DB995571EB220473FF92AD14D23'
+$url64          = 'https://download-cdn.resilio.com/stable/windows64/Resilio-Sync_x64.exe'
+$checksum64     = 'DC9C72E709CC7A2EA74999CE3756E6F4879CD6EAE50A888A1F8CA011A1D6EFA3'
+
+# SETTINGS:
+# Create Desktop icon (default: true)
+# Open Resilio Sync after installation (default: true)
+# Add an exception for Resilio Sync in Windows Firewall (default: true)
+# Start Resilio Sync when Windows starts up (default: true)
+# Install Resilio Sync as Windows service (default: false)
+# Was unable to find flags to change these
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  url64          = $url64
+  validExitCodes = @(0, 1)
+  silentArgs     = '/S'
+  softwareName   = 'Resilio Sync'
+  checksum       = $checksum
+  checksum64     = $checksum64
+  checksumType   = 'sha256' 
+  checksumType64 = 'sha256' 
+}
+
+Start-CheckandStop "Resilio Sync"
+Install-ChocolateyPackage @packageArgs
+Start-Sleep -s 5
+if ($ProcessWasRunning -eq $False) {Start-CheckandStop "Resilio Sync"}
