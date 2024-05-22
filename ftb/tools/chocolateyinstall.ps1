2234798099841e05a61f7cf7a52ac9bea5abd01b
Riley Nielsen <rileyjnielsen@gmail.com>
2024-05-22 10:51:27 -0700

    update ftb to 1.25.9

--- ftb/tools/chocolateyinstall.ps1
+++ ftb/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = '' # no 32-bit exe exists
-$url64      = 'https://piston.feed-the-beast.com/app/ftb-app-1.25.8-x64.exe'
+$url64      = 'https://piston.feed-the-beast.com/app/ftb-app-1.25.9-x64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -14,7 +14,7 @@ $packageArgs = @{
 
   checksum      = '' # no 32-bit exe exists
   checksumType  = 'sha256'
-  checksum64    = '0479501BF34E6278646CEB674A2196024D5EFB9C560630E1281C0FDDEB5C61EF'
+  checksum64    = '6B182BCEEC0E43912245BA8FE75B314884DAD866AABA235426DCF514F2DD0140'
   checksumType64= 'sha256'
 
   silentArgs   = '/S'
